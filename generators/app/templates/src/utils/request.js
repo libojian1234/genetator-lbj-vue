027768c0e7b309ad62dfc4c78c8dd45364de64ce
libojian <libojian1234@126.com>
2020-06-23 15:47:08 +0800

    修改多余逗号

--- generators/app/templates/src/utils/request.js
+++ generators/app/templates/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: process.env.VUE_APP_API_BASE,
+  baseURL: process.env.VUE_APP_API_BASE
   // auth: {
   //   username: 'frontend',
   //   password: 'ff6cf8d1-51c3-4dc6-8d3d-e1fd2cf48d31'
